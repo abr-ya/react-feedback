bd48375b0fb67deaed5c6a53c9a20d463193c07a
Abr-ya <gamer05@list.ru>
2022-03-26 07:31:46 +0300

    add eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:jest/recommended',
     'airbnb',
   ],
   parserOptions: {
@@ -17,9 +18,16 @@ module.exports = {
   },
   plugins: [
     'react',
+    'jest',
   ],
   rules: {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
+    // jest
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react on main page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const reactElement = screen.getByText(/react/i);
+  expect(reactElement).toBeInTheDocument();
 });
