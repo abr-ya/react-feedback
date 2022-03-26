06085676a0359c8330b00504914dd0e058ef8d46
abr-ya <gamer05@list.ru>
2022-03-26 14:07:01 +0300

    Add styled-components and base styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { StyledHeader } from './styled/Header.styled';
+
+function Header() {
+  return (
+    <StyledHeader>
+      <div className="container">
+        <h2>Feedback App</h2>
+      </div>
+    </StyledHeader>
+  );
+}
+
+export default Header;
