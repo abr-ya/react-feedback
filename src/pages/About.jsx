836a9315294855890820cb75b874837dd832da5b
abr-ya <gamer05@list.ru>
2022-04-15 20:05:30 +0300

    feat: Add Router

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,23 @@
-import { Container } from '../components/styled/Common.styled';
+import { Container, Card } from '../components/styled/Common.styled';
 
 function About() {
   return (
     <Container>
-      <h1>About page</h1>
+      <Card>
+        <h1>About page</h1>
 
-      <ul>
-        Стэк (основное):
-        <li>+ React</li>
-        <li>- TypeScript</li>
-        <li>+- Webpack (CRA)</li>
-        <li>- Redux</li>
-        <li>- Redux-Saga</li>
-        <li>+ React Router</li>
-        <li>- Axios</li>
-      </ul>
+        <ul>
+          Стэк (основное):
+          <li>+ React</li>
+          <li>- TypeScript</li>
+          <li>+- Webpack (CRA)</li>
+          <li>+ Styled Components</li>
+          <li>- Redux</li>
+          <li>- Redux-Saga</li>
+          <li>+ React Router</li>
+          <li>- Axios</li>
+        </ul>
+      </Card>
     </Container>
   );
 }
